9486efeaf4385df9d863b57b7d01c5aa022071e8
Amon Sharma <159574376+AmonSharma2004@users.noreply.github.com>
2024-07-30 08:21:54 +0530

    contact

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -90,13 +90,27 @@ export default function AboutMe  ()  {
           <div className='h-[60%] w-[100%] flex flex-col items-center justify-around my-5'>
           <div className='w-full h-[2px] bgcolor my-5'></div>
             <p className='text-[1.35rem] leading-snug  font-light max-[850px]:text-[1.1rem] my-5 font-extralight'>
-              <span className='first'>I</span> am <span className='bg-[var(--personaColor)] text-[var(--purpleColor)] font-bold'>Amon Sharma</span> from Vadodara currently pursuing B.Tech CSE in <span className=' bg-[var(--purpleColor)] text-[var(--personaColor)] font-bold'>IIIT Vadodara.</span> 
+            <span className='first'>I</span> am <span className='bg-[var(--personaColor)] text-[var(--purpleColor)] font-bold'>Amon Sharma</span> from Gujarat,India. Currently pursuing B.Tech in Computer Science and Engineering at <span className='bg-[var(--purpleColor)] text-[var(--personaColor)] font-bold'>IIIT Vadodara.</span>
             </p>
             <div className='w-full h-[.1px] bgcolor my-5'></div>
             <div className='text-[1.35rem] leading-snug font-light max-[850px]:text-[1.1rem] my-5 font-extralight'>
-              <span className='font-bold text-[2rem]'><span className='first'>Y</span>ou</span> can call me <span className='first font-bold'>Dzyno (or D-Zyner) </span>, not Dino because I am too old from my skills and young from my age and fitness. Jokes aside, I am a <span className='font-bold'><i>Techy</i></span>, <span className='font-bold'><i>Sleeky</i></span>, <span className='font-bold'><i>Visual</i></span>, <span className='font-bold'><i>Market</i></span>, <span className='font-bold'><i>Creation lover</i></span>. I love to create epic stories for your products, whether it's <span className='font-bold'><i>visually</i></span>, through <span className='font-bold'><i>video creation</i></span>, or as a <span className='font-bold'><i>content creator</i></span> for your next successful project. With expertise in <span className='font-bold'><i>Frontend Development</i></span>, <span className='font-bold'><i>UI/UX</i></span>, <span className='font-bold'><i>Graphics Design</i></span>, <span className='font-bold'><i>Marketing</i></span>, and <span className='font-bold'><i>Content Creation</i></span>, I can help bring your vision to life. 
-              <span className='bg-[var(--personaColor)] text-[var(--purpleColor)] font-bold'> Let's collaborate </span> and make your brand stand out in the <span className='font-black'>market!</span> 
-            </div>
+            
+ <span className='font-bold'><span className='first'>Y</span>ou can call me </span>  
+<span className='first font-bold'> Dzyno (or D-Zyner/Dezigner )</span>, not Dino. I am a 
+<span className='font-bold'><i>Tech and Coding Enthusiast</i></span>, 
+<span className='font-bold'><i>Sleek Designer</i></span>, 
+<span className='font-bold'><i>Visual Artist</i></span>, 
+<span className='font-bold'><i>Marketing Expert</i></span>, and 
+<span className='font-bold'><i>Creative Mind</i></span>. 
+I love to craft compelling stories for your products, whether visually or by Coding stuffs, through video creation, or as a content creator for your next successful project. With expertise in 
+<span className='font-bold'><i>Frontend Development</i></span>, 
+<span className='font-bold'><i>UI/UX Design</i></span>, 
+<span className='font-bold'><i>Graphic Design</i></span>, 
+<span className='font-bold'><i>Marketing</i></span>, and 
+<span className='font-bold'><i>Content Creation</i></span>, I can help bring your vision to life. 
+<span className='bg-[var(--personaColor)] text-[var(--purpleColor)] font-bold'> Let's collaborate </span> and make your brand stand out in the 
+<span className='font-black'>market!</span>
+ </div>
             <div className='w-full h-[2px] bgcolor my-5'></div>
             <div className='design h-[65vw] w-[44vw] absolute translate-y-[0vh] -z-10 max-[850px]:translate-y-[50vh] max-[850px]:h-[55vh] max-[850px]:w-[100vw] right-0 top-0'>
           <Image
